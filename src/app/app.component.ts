5c925819520f797d3e6ff85861384c9905c583ea
Evgenii Butkov <eugenefromrus@gmail.com>
2018-03-14 19:58:15 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // селектор
   selector: 'app-root',
+  // ссылка до шаблона данного селектора
   templateUrl: './app.component.html',
+  // массив ссылок до стилей
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'World!';
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  /**
+   * два обязательный свойста
+   */
+    selector: 'app-user',
+    templateUrl: './user.component.html'
+
+})
+export class UserComponent{
+
+}
